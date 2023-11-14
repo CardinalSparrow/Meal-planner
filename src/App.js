dd78a3f3e191f4f16f15ebf91bbdd5a58ee1fd96
Cardinal <Cardinalsparrow@gmail.com>
2023-11-14 04:53:04 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import FoodPlannerApp from './Meal';
 
 function App() {
   return (
     <div className="App">
-      <FoodPlannerApp/>
     </div>
   );
 }
