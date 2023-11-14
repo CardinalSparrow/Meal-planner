cce42f79c3f38685cd2c4be3a85dfbfbcc7a23e3
Cardinal <Cardinalsparrow@gmail.com>
2023-11-14 04:44:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FoodPlannerApp from './Meal';
+
+function App() {
+  return (
+    <div className="App">
+      <FoodPlannerApp/>
+    </div>
+  );
+}
+
+export default App;
